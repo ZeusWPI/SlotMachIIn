964214e43f906d211064e53647c90885179357df
hannes-kl <hannes.klinckaert@ugent.be>
2018-11-21 22:59:19 +0100

    Added TODO & changed serial prints

--- Arduino/SerialReadTest.ino
+++ Arduino/SerialReadTest.ino
@@ -4,8 +4,12 @@ void setup() {
 
 bool openDoor(){
  return true;
+ //TODO: control motor to open door
+ //Return true if it was able to open the door
   }
 bool closeDoor(){
+ //TODO: control motor to close door
+ //Return true if it was able to close the door
  return false;
   }
 
@@ -13,17 +17,20 @@ void loop() {
   if (Serial.available() > 0) {
    String tekst = Serial.readString();
    tekst.trim();
+   //Check Serial Input
+    //If input is "open", open the door.
    if (tekst == "open"){   
     if(openDoor()){
-      Serial.println("Gelukt!");
+      Serial.println("open");
     } else {
-      Serial.println("Niet gelukt!");
+      Serial.println("error");
     }
+    //If input is "close", close the door.
    } else if(tekst == "close"){
     if(closeDoor()){
-     Serial.println("Gelukt!");
+     Serial.println("close");
     } else {
-     Serial.println("Niet gelukt!");
+     Serial.println("error");
     }
   }
  }
