7480ca965ddd358ea34938c6a0033e2a8771444a
ajuvercr <arthur.vercruysse@outlook.com>
2018-11-25 21:32:12 +0100

    added untested arduino code, should work

---
+++ Arduino/OpenAndCloseFUCKINGDoor.ino
@@ -0,0 +1,133 @@
+
+/*
+  FUCKING FUCK THIS FUCKIN KAK SHIT
+*/
+
+// ananogWrite value between 0 and 255 (8 bit value)
+#define MOTOR_PWM 9
+#define SER1 2
+#define SER2 3
+#define ROT_CLOCK 5
+#define ROT_CCLOCK 4
+
+#define OPEN_PIN 6
+#define CLOSE_PIN 7
+
+#define IS_OPEN_PIN 10
+#define IS_CLOSE_PIN 11
+
+// Half the max speed
+#define TURN_SPEED 128
+
+#define GETSER1 (digitalRead(SER1) == HIGH)
+#define GETSER2 (digitalRead(SER2) == HIGH)
+
+#define OPEN true
+#define CLOSE false
+
+// TODO, experiment!
+#define CLOSE_THRESHHOLD 50
+#define OPEN_THRESHHOLD 100
+                                // 11         10              00              01
+//const TurnTuple cwSequence[] = {{true, true}, {true, false}, {false, false}, {false, true}};
+const int pulses_per_turn = 180; // http://trivox.tripod.com/lego-nxt-motor-input-output.html
+
+volatile int current_turn_count = 0;
+
+volatile bool prev_ser1, prev_ser2 = false;
+
+bool is_open = false;
+
+void init_motor_pinout() {
+  pinMode(MOTOR_PWM, OUTPUT);
+  pinMode(SER1, INPUT);
+  pinMode(SER2, INPUT);
+  pinMode(ROT_CLOCK, OUTPUT);
+  pinMode(ROT_CCLOCK, OUTPUT);
+}
+
+int is_turning_clockwise(bool ser1, bool ser2) {
+   if(ser1 == prev_ser1) {
+    return (prev_ser2 && !ser2) || (!prev_ser2 && ser2);
+   } else {
+    return (prev_ser1 && !ser1) || (!prev_ser1 && ser1);
+   }
+}
+
+void update_ser1(void) {
+  current_turn_count += is_turning_clockwise(GETSER1, prev_ser2);
+  prev_ser1 = GETSER1;
+}
+
+void update_ser2(void) {
+  current_turn_count += is_turning_clockwise(prev_ser1, GETSER2);
+  prev_ser1 = GETSER2;
+}
+
+void turn(bool clockwise) {
+  digitalWrite(ROT_CLOCK, LOW);
+  digitalWrite(ROT_CCLOCK, LOW);
+  
+  if(clockwise) {
+    digitalWrite(ROT_CLOCK, HIGH);
+  } else {
+    digitalWrite(ROT_CCLOCK, HIGH);
+  }
+
+  analogWrite(MOTOR_PWM, TURN_SPEED);
+}
+
+void stop_turn(void) {
+   analogWrite(MOTOR_PWM, 0);
+}
+
+void turn_total(bool dir) {
+  int prev_state = current_turn_count;
+  turn(dir);
+  delay(100);
+  
+  while(prev_state != current_turn_count) {
+    prev_state = current_turn_count;
+    delay(100);
+  }
+  
+  stop_turn();
+}
+
+void determine_start(void) {
+  turn_total(CLOSE);
+  
+  current_turn_count = 0;
+  is_open = false;
+}
+
+void setup() {
+  init_motor_pinout();
+  pinMode(OPEN_PIN, INPUT);
+  pinMode(CLOSE_PIN, INPUT);
+  pinMode(IS_OPEN_PIN, OUTPUT);
+  pinMode(IS_CLOSE_PIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(SER1), update_ser1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SER2), update_ser2, CHANGE);
+
+  determine_start();
+}
+
+void loop() {
+  bool want_open = digitalRead(OPEN_PIN) == HIGH;
+  bool want_closed = digitalRead(CLOSE_PIN) == HIGH;
+
+  if (want_open != want_closed) {
+    if (want_open && current_turn_count < OPEN_THRESHHOLD) {
+      turn_total(OPEN);
+    } 
+    
+    if (want_closed && current_turn_count > CLOSE_THRESHHOLD) {
+       turn_total(CLOSE);
+    }
+  }
+
+  delay(100);
+}
+
+
